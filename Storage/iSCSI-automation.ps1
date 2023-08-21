dac6debf0bca0c36342e6edb9caac559b4b0db23
Joseph Bollman <jbollman@protonmail.com>
2023-08-21 14:53:20 -0600

    Update iSCSI-automation.ps1
    Added Get and Set iSCSI Targets

--- Storage/iSCSI-automation.ps1
+++ Storage/iSCSI-automation.ps1
@@ -1,5 +1,5 @@
 # Enabling an iSCSI software adapter for all hosts in a cluster
-function SetClusterIscsi {
+function SetClusterIscsiAdapter {
     param(
         [string] $clusterName
     )
@@ -80,3 +80,100 @@ function AddIscsiTarget {
         Write-Error "An error occurred while processing cluster '$clusterName': $_"
     }
 }
+
+# This function retrieves the iSCSI HBA target for a specified cluster, iSCSI HBA device, and iSCSI HBA target address
+function GetClusterIscsi {
+    param(
+        [string] $clusterName,
+        [string] $iScsiHbaDevice,
+        [string] $iScsiHbaTargetAddress
+    )
+    try {
+        $cluster = Get-Cluster $clusterName
+        if (!$cluster) {
+            Write-Error "Cluster '$clusterName' not found"
+            return
+        }
+
+        $hosts = $cluster | Get-VMHost
+        if (!$hosts) {
+            Write-Error "No hosts found in cluster '$clusterName'"
+            return
+        }
+
+        foreach ($host in $hosts) {
+            try {
+                # Get the iSCSI HBA device
+                $iScsiHba = $host | Get-VMHostHba -Type iScsi | Where-Object {$_.Device -eq $iScsiHbaDevice}
+                if (!$iScsiHba) {
+                    Write-Warning "iSCSI HBA device '$iScsiHbaDevice' not found for host '$($host.Name)'"
+                    continue
+                }
+
+                # Get the iSCSI HBA target
+                $iScsiHbaTarget = $iScsiHba | Get-IScsiHbaTarget | Where-Object {$_.Address -eq $iScsiHbaTargetAddress}
+                if (!$iScsiHbaTarget) {
+                    Write-Warning "iSCSI HBA target with address '$iScsiHbaTargetAddress' not found for host '$($host.Name)'"
+                    continue
+                }
+
+                # Output the iSCSI HBA target information
+                Write-Output "Found iSCSI HBA target with address '$iScsiHbaTargetAddress' for host '$($host.Name)'"
+            } catch {
+                Write-Error "An error occurred while retrieving iSCSI HBA target for host '$($host.Name)': $_"
+            }
+        }
+    } catch {
+        Write-Error "An error occurred while processing cluster '$clusterName': $_"
+    }
+}
+
+# This function sets the iSCSI HBA target for a specified cluster, iSCSI HBA device, and iSCSI HBA target address
+# Retrieves the specified cluster and its hosts. for each host, retrieves the specified iSCSI HBA device and its target, enbles the target with `Set-ISCSIHbaTarget`
+function SetClusterIscsi {
+    param(
+        [string] $clusterName,
+        [string] $iScsiHbaDevice,
+        [string] $iScsiHbaTargetAddress
+    )
+    try {
+        $cluster = Get-Cluster $clusterName
+        if (!$cluster) {
+            Write-Error "Cluster '$clusterName' not found"
+            return
+        }
+
+        $hosts = $cluster | Get-VMHost
+        if (!$hosts) {
+            Write-Error "No hosts found in cluster '$clusterName'"
+            return
+        }
+
+        foreach ($host in $hosts) {
+            try {
+                # Get the iSCSI HBA device
+                $iScsiHba = $host | Get-VMHostHba -Type iScsi | Where-Object {$_.Device -eq $iScsiHbaDevice}
+                if (!$iScsiHba) {
+                    Write-Warning "iSCSI HBA device '$iScsiHbaDevice' not found for host '$($host.Name)'"
+                    continue
+                }
+
+                # Get the iSCSI HBA target
+                $iScsiHbaTarget = $iScsiHba | Get-IScsiHbaTarget | Where-Object {$_.Address -eq $iScsiHbaTargetAddress}
+                if (!$iScsiHbaTarget) {
+                    Write-Warning "iSCSI HBA target with address '$iScsiHbaTargetAddress' not found for host '$($host.Name)'"
+                    continue
+                }
+
+                # Set the iSCSI HBA target
+                $iScsiHbaTarget | Set-IScsiHbaTarget -Enabled:$true
+                Write-Output "Enabled iSCSI HBA target with address '$iScsiHbaTargetAddress' for host '$($host.Name)'"
+            } catch {
+                Write-Error "An error occurred while enabling iSCSI HBA target for host '$($host.Name)': $_"
+            }
+        }
+    } catch {
+        Write-Error "An error occurred while processing cluster '$clusterName': $_"
+    }
+}
+
